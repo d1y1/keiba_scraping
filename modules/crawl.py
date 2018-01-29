02d18a97837edf0d3ba5da0722a11a10e0c904ad
yonemura <yonemura@arara.com>
2018-01-29 12:53:59 +0900

    add fix

---
+++ modules/crawl.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import re
+import scrapy
+
+# 参考URL(Scrapy Tutorial)
+# https://doc.scrapy.org/en/latest/intro/tutorial.html
+
+# 使い方
+# scrapy file name
+# 例
+# scrapy crawl multiple
--- modules/scraping.py
+++ modules/scraping.py
@@ -34,29 +34,28 @@ def get_text(class_name):
     return list
 
 
+if __name__ == '__main__':
 # 本レースデータの取得
-race_data = {}
-race_row_data = crawling(race_url)
-race_data['num'] = race_row_data.find(attrs={"class": "Race_Num"}).text.lstrip().rstrip()
-race_data['name'] = race_row_data.find(attrs={"class": "Race_Name"}).text.rstrip()
-course_data = race_row_data.find(attrs={"class": "Race_Data"}).span.text.rstrip()
-race_data['range'] = re.search('\d+.', course_data).group()
-race_data['type'] = re.search('(芝|ダート)', course_data).group()
-
-# 出場馬のリスト
-horse_list = get_text('.Horse')
-
-horse_link = []
-horse_row_link_list = race_row_data.find_all(attrs={"class": "Horse"})
-for row in horse_row_link_list:
-    href = row.a.get('href')
-    horse_link.append(href)
-
-horse_url_dict = dict(zip(horse_list, horse_link))
-
-print('レース情報:')
-print(race_data)
-print('出走馬:')
-print(horse_list)
-print(horse_link)
-print(horse_url_dict)
+    race_data = {}
+    race_row_data = crawling(race_url)
+    race_data['num'] = race_row_data.find(attrs={"class": "Race_Num"}).text.lstrip().rstrip()
+    race_data['name'] = race_row_data.find(attrs={"class": "Race_Name"}).text.rstrip()
+    course_data = race_row_data.find(attrs={"class": "Race_Data"}).span.text.rstrip()
+    race_data['range'] = re.search('\d+.', course_data).group()
+    race_data['type'] = re.search('(芝|ダート)', course_data).group()
+    # 出場馬のリスト
+    horse_list = get_text('.Horse')
+    # 出場馬とリンクのディクショナリー
+    horse_link = []
+    horse_row_link_list = race_row_data.find_all(attrs={"class": "Horse"})
+    for row in horse_row_link_list:
+        href = row.a.get('href')
+        horse_link.append(href)
+    horse_url_dict = dict(zip(horse_list, horse_link))
+    
+    print('レース情報:')
+    print(race_data)
+    print('出走馬:')
+    print(horse_list)
+    print(horse_link)
+    print(horse_url_dict)
