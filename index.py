36ad122d5cba52a3401cc65d513211249e074118
yonemura <yonemura@arara.com>
2018-01-29 13:01:56 +0900

    edit routing

--- index.py
+++ index.py
@@ -5,26 +5,25 @@ from bottle import route, run, template, request
 from modules import *
 
 # localhost:8080
-@route('/')
+@route('/check')
 def title():
-    # views/title.tplを呼ぶ
-    return template('title')
+    # views/check.tplを呼ぶ
+    return template('check')
 
 
 # localhost:8080/show
-@route('/show', method='GET')
+@route('/check', method='GET')
 def men():
-    # GETパラメータの取得(username, men)
+    # GETパラメータの取得 =====================================
     race_url = request.query.url
 
     # Controller部 =======================================
     data = scraping.crawling(race_url)
     
-    
     # View部 =============================================
-    # views/show.tplを呼ぶ
-    return template('show', data=data)
+    # views/check.tplを呼ぶ
+    return template('check', data=data)
 
 
-# ビルドインサーバの実行
+# ビルドインサーバの実行 ========================================
 run(host='d1y1.net', port=8080, debug=True, reloader=True)
---
+++ views/check.tpl
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang=ja>
+  <head>
+    <meta charset="UTF-8">
+    <title>タイトルページ</title>
+  </head>
+
+  <body>
+    <h1>タイトルページ</h1>
+    <h2>URL入力</h2>
+    <form method="GET" action="/show">
+    <input type="text" name="url"></p>
+    <input type="submit" value="送信する">
+    </form>
+
+    <h2>結果出力のページ</h2>
+    <p>{{data}}<p>
+    <a href="/">return to HOME</a>
+    </p>
+
+  </body>
+
+</html>
