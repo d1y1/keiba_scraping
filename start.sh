d585fdc6ea65bd59f54558ef8be58210bd19a6f8
d1y1 <yone.d1y1.net>
2018-01-30 15:56:32 +0900

    edit start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-killall -9 python3\ index.py
+kill -9 `cat /var/log/save_pid.txt`
+rm /var/log/save_pid.txt
 cd /root/python
 git pull origin master
 nohup python3 index.py > /var/log/nohup.log &
+echo $! > /var/log/save_pid.txt
